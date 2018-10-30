c96c1367e7defc5d79f166a4d9357c26a00ee67e
dwdraju <rajudawadinp@gmail.com>
2018-10-30 17:02:03 +0545

    init module

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "google_compute_instance" "this" {
+  name         = "${var.name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+
+  tags = ["${var.tags}"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  scratch_disk {}
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "${var.metadata_startup_script}"
+
+  service_account {
+    scopes = ["${var.sa_scopes}"]
+  }
+
+  scheduling {
+    preemptible       = "${var.preemptible}"
+    automatic_restart = "${var.preemptible != "true" ? false : true}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  description = "Name to use for the instance"
+  default     = ""
+}
+
+variable "machine_type" {
+  description = "Instance type of instance, eg. n1-standard-1"
+  default     = "n1-standard-1"
+}
+
+variable "zone" {
+  description = "Availibility zone of the instance"
+  default     = "us-central1-b"
+}
+
+variable "image" {
+  description = "Image to use for the instance, could be custom or public"
+  default     = "ubuntu-os-cloud/ubuntu-1604-lts"
+}
+
+variable "tags" {
+  description = "List of tags to use for the instance"
+  default     = []
+}
+
+variable "preemptible" {
+  description = "Flag for preemptibility of instance. If true, the instance shuts down within 24 hours"
+  default     = "false"
+}
+
+variable "metadata_startup_script" {
+  description = "Startup script"
+  default     = ""
+}
+
+variable "sa_scopes" {
+  description = "Service account scope for the instance"
+  default     = ["userinfo-email", "compute-ro", "storage-ro"]
+}
